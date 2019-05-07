b503dcc9f11521b8f78fbb56f25b8299a939a075
mann <joshuadjoffe1@gmail.com>
2019-05-07 11:52:51 +1000

    2

---
+++ Sunnyland/Assets/Scri/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//important
+using Prime31;
+public class Player : MonoBehaviour
+{
+    public float gravity = -10f;
+    public float movespeed = 10f;
+    private CharacterController2D controller;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        controller.move(transform.right * inputH * movespeed * Time.deltaTime);
+    }
+}
