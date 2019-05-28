996793cac0873779b2238fbe047141751e3ea6d5
josh <48034855+JDJoffe@users.noreply.github.com>
2019-05-28 12:07:23 +1000

    character animations

--- Sunnyland/Assets/Scri/Player.cs
+++ Sunnyland/Assets/Scri/Player.cs
@@ -9,13 +9,16 @@ public class Player : MonoBehaviour
     public float gravity = -10f;
     public float movespeed = 5f;
     private CharacterController2D controller;
-
+    private Animator anim;
+    private SpriteRenderer Renderer;
 
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController2D>();
+        anim = GetComponent<Animator>();
+        Renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,6 +27,16 @@ public class Player : MonoBehaviour
         float inputH = Input.GetAxis("Horizontal");
         float inputV = Input.GetAxis("Vertical");
 
+        Move(inputH, inputV);
+
+       
+    }
+    void Move(float inputH, float inputV)
+    {
         controller.move(transform.right * inputH * movespeed * Time.deltaTime);
+        bool isRunning = inputH != 0;
+            anim.SetBool("IsRunning", isRunning);
+        Renderer.flipX = inputH > 0;
+       
     }
 }
