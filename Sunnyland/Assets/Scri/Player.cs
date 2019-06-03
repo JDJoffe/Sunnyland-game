7acc48b2a0e48f5e31d482ca8cbb090bdfbb85ac
JDJoffe <48034855+JDJoffe@users.noreply.github.com>
2019-06-03 12:01:17 +1000

    jump anim, ladders, camera follow, almost climbing

--- Sunnyland/Assets/Scri/Player.cs
+++ Sunnyland/Assets/Scri/Player.cs
@@ -8,35 +8,109 @@ public class Player : MonoBehaviour
 {
     public float gravity = -10f;
     public float movespeed = 5f;
-    private CharacterController2D controller;
+    public float jumpHeight = 3.75f;
+    public float centreRadius = 0.1f;
+    private CharacterController2D controller;   
     private Animator anim;
-    private SpriteRenderer Renderer;
-
-
+    private SpriteRenderer renderer;
+    private Vector3 velocity;
+  private  bool isClimbing = false;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, centreRadius);
+    }
     // Start is called before the first frame update
     void Start()
     {
+        //getcomponents
         controller = GetComponent<CharacterController2D>();
         anim = GetComponent<Animator>();
-        Renderer = GetComponent<SpriteRenderer>();
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       //get left and right movement
         float inputH = Input.GetAxis("Horizontal");
+        //gets up and down movement
         float inputV = Input.GetAxis("Vertical");
 
-        Move(inputH, inputV);
+        //grounded check
+        if (!controller.isGrounded)
+        {
+            //apply time to gravity
+            velocity.y += gravity * Time.deltaTime;
+        }
+        
+        //get spacebar inpug
+        bool isJumping = Input.GetButtonDown("Jump");
 
-       
+        if (isJumping)
+        {
+            //character jump
+            Jump();
+           
+        }
+        //unity bool == script bool
+        anim.SetBool("IsGrounded", controller.isGrounded);
+        //unity float == velocity float
+        anim.SetFloat("JumpY", velocity.y);
+
+        //call
+        Move(inputH);
+        Climb(inputV);
+
+        //applies velocity to controller to make it move
+        controller.move(velocity * Time.deltaTime);
     }
-    void Move(float inputH, float inputV)
+
+    void Move(float inputH )
     {
-        controller.move(transform.right * inputH * movespeed * Time.deltaTime);
+        //left and right movement
+        velocity.x = inputH * movespeed;
+      
+        //bool isrunning = true when inputH is not zero
         bool isRunning = inputH != 0;
+        //animate if move button is pressed
             anim.SetBool("IsRunning", isRunning);
-        Renderer.flipX = inputH > 0;
+        
+        // flip the sprite when you are moving in the oposite direction
+        if (isRunning) { renderer.flipX = inputH < 0; }
+      
+    }
+
+    void Climb(float inputV)
+    {
+        bool isOverLadder = false; //overlapping a ladder
+        
        
+        //get a list of hit objects overlapping point
+        Collider2D[] hits = Physics2D.OverlapPointAll(transform.position);
+        //loop through each point
+        foreach(var hit in hits)
+        {
+            //if point overlaps climbable object
+            if (hit.tag == "Ladder")
+            {
+                //player overlap ladder
+                isOverLadder = true;
+                break;
+            }
+        }
+        //if does & inputV has been made
+        if (isOverLadder && inputV !=0)
+        {
+            //climb
+            isClimbing = true;
+        }
+       //do logic for climbing
+    }
+    void Jump()
+    {
+        // set velocity y index to height
+        velocity.y = jumpHeight;
+
     }
 }
